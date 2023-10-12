24a113f001e4ec8416f8a43b8b21ac0b547c07eb
thelinus1997 <thelinus1997@gmail.com>
2023-10-12 08:47:01 +0200

    fixat regex

--- script.js
+++ script.js
@@ -32,10 +32,14 @@ for (let i = 0; i < wordToGuess.length; i++) {
   document.getElementById("wordToGuess").appendChild(wordBox);
 }
 //Kontrollerar inputvärde så att det enbart är en bokstav och inte en siffra och kör sedan jämförelsen
-function checkIfLetter(input){
-    if(!/\d/.test(input) && typeof input === "string" && input.length === 1){
-        letterComparision(input)
-    }
+function checkIfLetter(input) {
+  if (
+    /[A-Za-z]/.test(input) &&
+    typeof input === "string" &&
+    input.length === 1
+  ) {
+    letterComparision(input);
+  }
 }
 
 //Skapa funktion som kontrollerar om bokstaven stämmer eller inte.
@@ -51,66 +55,64 @@ function letterComparision(input) {
       const lettersToWrite = document.querySelectorAll(`.${wordToGuess[i]}`);
       lettersToWrite.forEach((letter) => {
         letter.innerText = wordToGuess[i];
-        correctGuesses.push(wordToGuess[i])
+        correctGuesses.push(wordToGuess[i]);
       });
-      
-      if(correctGuesses.length == wordToGuess.length ){
-        let winWindow = document.createElement('div')
-        winWindow.classList.add("winWindow")
-        
-        let winImage = document.createElement("img")
-        winImage.src = "giphy.gif"
-        winWindow.appendChild(winImage)
-        let winMessage = document.createElement('h2')
-        winMessage.innerText = "YOU WIN!"
-        winWindow.appendChild(winMessage)
-        let playAgainButton = document.createElement('button')
-        playAgainButton.innerText = 'play again'
-        playAgainButton.onclick = refreshSite
-        winWindow.appendChild(playAgainButton)
-        document.querySelector('body').appendChild(winWindow)
+
+      if (correctGuesses.length == wordToGuess.length) {
+        let winWindow = document.createElement("div");
+        winWindow.classList.add("winWindow");
+
+        let winImage = document.createElement("img");
+        winImage.src = "giphy.gif";
+        winWindow.appendChild(winImage);
+        let winMessage = document.createElement("h2");
+        winMessage.innerText = "YOU WIN!";
+        winWindow.appendChild(winMessage);
+        let playAgainButton = document.createElement("button");
+        playAgainButton.innerText = "play again";
+        playAgainButton.onclick = refreshSite;
+        winWindow.appendChild(playAgainButton);
+        document.querySelector("body").appendChild(winWindow);
       }
       break;
     }
   }
-if(!doesLetterExist && !guessedLetters.includes(input)){
-    
- let newWrongLetter = document.createElement("h2")
- newWrongLetter.innerText = input
- document.getElementById("wrongGuesses").appendChild(newWrongLetter);
- guessedLetters.push(input)
+  if (!doesLetterExist && !guessedLetters.includes(input)) {
+    let newWrongLetter = document.createElement("h2");
+    newWrongLetter.innerText = input;
+    document.getElementById("wrongGuesses").appendChild(newWrongLetter);
+    guessedLetters.push(input);
 
- document.getElementById(hangMan[numberOfWrongGuesses]).style.display = "block";
+    document.getElementById(hangMan[numberOfWrongGuesses]).style.display =
+      "block";
 
- numberOfWrongGuesses ++ // Ökar antalet felgissningar
+    numberOfWrongGuesses++; // Ökar antalet felgissningar
 
- if(numberOfWrongGuesses == hangMan.length){
-    console.log('något')
-    let loseWindow = document.createElement('div')
-    loseWindow.classList.add("winWindow")
-        
-        let loseImage = document.createElement("img")
-        loseImage.src = "giphy.gif"
-        loseWindow.appendChild(loseImage)
-        let loseMessage = document.createElement('h2')
-        loseMessage.innerText = "YOU LOSE!"
-        let correctWord = document.createElement('p')
-        correctWord.innerText = "The word was: " + wordToGuess.join('')
-        loseWindow.appendChild(loseMessage)
-        loseWindow.appendChild(correctWord)
-        let playAgainButton = document.createElement('button')
-        playAgainButton.innerText = 'play again'
-        playAgainButton.onclick = refreshSite
-        loseWindow.appendChild(playAgainButton)
-        document.querySelector('body').appendChild(loseWindow)
-      
-   
-}
-}
+    if (numberOfWrongGuesses == hangMan.length) {
+      console.log("något");
+      let loseWindow = document.createElement("div");
+      loseWindow.classList.add("winWindow");
+
+      let loseImage = document.createElement("img");
+      loseImage.src = "giphy.gif";
+      loseWindow.appendChild(loseImage);
+      let loseMessage = document.createElement("h2");
+      loseMessage.innerText = "YOU LOSE!";
+      let correctWord = document.createElement("p");
+      correctWord.innerText = "The word was: " + wordToGuess.join("");
+      loseWindow.appendChild(loseMessage);
+      loseWindow.appendChild(correctWord);
+      let playAgainButton = document.createElement("button");
+      playAgainButton.innerText = "play again";
+      playAgainButton.onclick = refreshSite;
+      loseWindow.appendChild(playAgainButton);
+      document.querySelector("body").appendChild(loseWindow);
+    }
+  }
 }
 
-function refreshSite(){
-    window.location = "index.html"
+function refreshSite() {
+  window.location = "index.html";
 }
 //Om svaret stämmer skall bokstaven visas på korrekt plats i gissningslådan.
 
